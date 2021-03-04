70413581921248729edb4d461b86b3b467b9984e
Holly McQueary <hmcqueary@cornell.edu>
2021-03-04 12:22:23 -0500

    apparently can't use variables?

--- get_SRR_data.sh
+++ get_SRR_data.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 
-work_dir = "/workdir/hcm59/Ecoli/raw_data/PRJNA324565"
-mkdir ${workdir}
-cd ${workdir}
+mkdir /workdir/hcm59/Ecoli/raw_data/PRJNA324565
+cd /workdir/hcm59/Ecoli/raw_data/PRJNA324565
 
 fastq-dump --split-3 $1
 
 # to run:
-  # cat /workdir/hcm59/PRJNA324565_acc_list.txt | xargs -n 1 bash get_SRR_data.sh
+  # cat /workdir/hcm59/PRJNA324565_acc_list.txt | xargs -n 1 bash /workdir/hcm59/CornellPostdoc/get_SRR_data.sh
