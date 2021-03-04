324a1aa589ac068ab63579b2319fe7ffc1796983
Holly McQueary <hmcqueary@cornell.edu>
2021-03-04 12:45:44 -0500

    update for PRJNA318589

--- get_SRR_data.sh
+++ get_SRR_data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-mkdir /workdir/hcm59/Ecoli/raw_data/PRJNA503851
-cd /workdir/hcm59/Ecoli/raw_data/PRJNA503851
+mkdir /workdir/hcm59/Ecoli/raw_data/PRJNA318589
+cd /workdir/hcm59/Ecoli/raw_data/PRJNA318589
 
 fastq-dump --split-3 $1
 
 # to run:
-  # cat /workdir/hcm59/Ecoli/PRJNA503851_SraAccList.txt | xargs -n 1 bash /workdir/hcm59/CornellPostdoc/get_SRR_data.sh
+  # cat /workdir/hcm59/Ecoli/PRJNA318589_SraAccList.txt | xargs -n 1 bash /workdir/hcm59/CornellPostdoc/get_SRR_data.sh
