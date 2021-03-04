fc569c952af48d2c79e42a3af2b954ef5c55a94f
Holly McQueary <hmcqueary@cornell.edu>
2021-03-04 12:36:21 -0500

    update for new SRR number

--- get_SRR_data.sh
+++ get_SRR_data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-mkdir /workdir/hcm59/Ecoli/raw_data/PRJNA324565
-cd /workdir/hcm59/Ecoli/raw_data/PRJNA324565
+mkdir /workdir/hcm59/Ecoli/raw_data/PRJNA324573
+cd /workdir/hcm59/Ecoli/raw_data/PRJNA324573
 
 fastq-dump --split-3 $1
 
 # to run:
-  # cat /workdir/hcm59/PRJNA324565_acc_list.txt | xargs -n 1 bash /workdir/hcm59/CornellPostdoc/get_SRR_data.sh
+  # cat /workdir/hcm59/Ecoli/PRJNA324573_SraAccList.txt | xargs -n 1 bash /workdir/hcm59/CornellPostdoc/get_SRR_data.sh
