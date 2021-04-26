d585a691fb55284c712641f215d336933f7b2bec
Holly McQueary <hmcqueary@cornell.edu>
2021-04-26 17:38:26 -0400

    misc scripts + download 601 dog e coli seqs

--- get_SRR_data.sh
+++ get_SRR_data.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 
-mkdir /workdir/hcm59/Ecoli/raw_data/PRJNA318589
-cd /workdir/hcm59/Ecoli/raw_data/PRJNA318589
+cd /workdir/hcm59/Ecoli/SNPs/raw_fastqs
 
 fastq-dump --split-3 $1
 
 # to run:
-  # cat /workdir/hcm59/Ecoli/SraAccList_PRJNA318589.txt| xargs -n 1 bash /workdir/hcm59/CornellPostdoc/get_SRR_data.sh
+  # cat /workdir/hcm59/Ecoli/phenoDataSamplesAcc_short.txt| xargs -n 1 bash /workdir/hcm59/CornellPostdoc/get_SRR_data.sh
--- misc_scripts.sh
+++ misc_scripts.sh
@@ -5,3 +5,7 @@ grep "^>" myfile.fasta
 
 ## To compare if two fasta files have the same headers:
 awk '/^>/{if (a[$1]>=1){print $1}a[$1]++}' file1 file2
+
+
+# to compare two files and print matches
+awk 'NR==FNR {end[$1]; next} ($2 in end)' phenoDataSamples.txt  allSamples.txt > phenoDataSamplesAcc.txt
