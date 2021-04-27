d822ffc1276b694904e12b43c92189e177fda6b0
Holly McQueary <hmcqueary@cornell.edu>
2021-04-27 16:35:24 -0400

    getting srr data for amrfinder

--- get_SRR_data.sh
+++ get_SRR_data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-cd /workdir/hcm59/Ecoli/SNPs/raw_fastqs
+cd /workdir/hcm59/Ecoli/raw_data/PRJNA318589
 
 fastq-dump --split-3 $1
 
 # to run:
-  # cat /workdir/hcm59/Ecoli/phenoDataSamplesAcc_short.txt| xargs -n 1 bash /workdir/hcm59/CornellPostdoc/get_SRR_data.sh
+  # cat /workdir/hcm59/Ecoli/PRJNA318589_SraAccList.txt| xargs -n 1 bash /workdir/hcm59/CornellPostdoc/get_SRR_data.sh
