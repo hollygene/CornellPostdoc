ed6913d790788b1de23784281e475dce1878d1bd
Holly McQueary <hmcqueary@cornell.edu>
2021-05-19 15:03:19 -0400

    socary viz + getting rest of 601 samples for AMRFinder

--- get_SRR_data.sh
+++ get_SRR_data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-cd /workdir/hcm59/Ecoli/SNPs/GATK_SNP_calling
+cd /workdir/hcm59/Ecoli/raw_data/rest_of_601
 
 fastq-dump --split-3 $1
 
 # to run:
-  # cat /workdir/hcm59/Ecoli/phenoDataSamplesAcc_short.txt| xargs -n 1 bash /workdir/hcm59/CornellPostdoc/get_SRR_data.sh
+  # cat /workdir/hcm59/Ecoli/missingfrom601.txt | xargs -n 1 bash /workdir/hcm59/CornellPostdoc/get_SRR_data.sh
