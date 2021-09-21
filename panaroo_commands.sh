9320c4d22d28619b8e4c35f0941bb1119ec16887
Holly McQueary <hmcqueary@cornell.edu>
2021-09-21 15:29:39 -0400

    scripts to get panaroo up and running

--- panaroo_commands.sh
+++ panaroo_commands.sh
@@ -1,9 +1,16 @@
 # panaroo commands for dog E coli stuff
+# set environment
+export PATH=/programs/cd-hit-4.8.1:/programs/mafft/bin:$PATH
+export PATH=/programs/panaroo-1.2.3/bin:$PATH
+export PYTHONPATH=/programs/panaroo-1.2.3/lib/python3.6/site-packages:/programs/panaroo-1.2.3/lib64/python3.6/site-packages
 
+gff_dir="/workdir/hcm59/Ecoli/prokka_gffs"
+results_dir="/workdir/hcm59/Ecoli"
+mkdir ${results_dir}
 
 # run panaroo and remove invalid genes (those with stop codons, etc), using 10 threads:
-panaroo -i *.gff -o results_verified_host --clean-mode strict --remove-invalid-genes -t 10
+panaroo -i ${gff_dir}/*.gff -o ${results_dir} --clean-mode strict --remove-invalid-genes -t 10
 
 
-# get a core gene alignment with the built in mafft aligner, output to the current directory:
-panaroo-msa --aligner mafft -a core  -t 20 --verbose -o .
+# get a core gene alignment with the built in mafft aligner, output to the results directory
+panaroo-msa --aligner mafft -a core  -t 20 --verbose -o ${results_dir}
