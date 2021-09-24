4a697abd0cc02501a37ae93ad2a525015f809423
Holly McQueary <hmcqueary@cornell.edu>
2021-09-24 15:19:10 -0400

    ncbi datasets cli commands

---
+++ ncbi_datasets_cli.sh
@@ -0,0 +1,20 @@
+# use ncbi's datasets tool to download assemblies
+
+wget https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-amd64/datasets
+
+chmod a+x datasets
+
+# ./datasets
+
+#for example, download gene with gene-id 672
+# ./datasets download gene GCA_012348725.1
+
+
+#!/bin/bash
+filename='/workdir/hcm59/Ecoli/200_contigs_or_less.txt'
+while read p; do
+    echo "$p"
+    # ./datasets download gene $p
+done < "$filename"
+
+# datasets download genome accession GCF_000001405.39 --dehydrated --filename human_GRCh38_dataset.zip
